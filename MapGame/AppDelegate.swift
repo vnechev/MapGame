224ddc553929697a23e354f07574c80326c269d0
Vasil Nechev <vnechev@gmail.com>
2019-02-21 13:31:27 +0200

    Initial Commit

---
+++ MapGame/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MapGame
+//
+//  Created by Vasil Nechev on 21.02.19.
+//  Copyright © 2019 Vasil Nechev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MapGame/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MapGame
+//
+//  Created by Vasil Nechev on 21.02.19.
+//  Copyright © 2019 Vasil Nechev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
